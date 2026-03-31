679de202803e2a084bea8cbaaa89b27406f85e9f
facussc24 <facussc24@users.noreply.github.com>
2026-03-31 10:51:30 -0300

    Updates

--- assets/crypto-CYAKiIwB.js
+++
@@ -1,1 +0,0 @@
-async function s(r){const t=new TextEncoder().encode(r),e=await crypto.subtle.digest("SHA-256",t);return Array.from(new Uint8Array(e)).map(a=>a.toString(16).padStart(2,"0")).join("")}export{s as g};
